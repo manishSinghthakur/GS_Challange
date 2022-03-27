87fade6b7ceacacc6f93116d301dc2d59907b504
Manish Singh <manish.singh705@gmail.com>
2022-03-27 19:45:04 +0530

    Dark Mode Support

--- GS-CodingChallenge/AppSetup/GS_CodingChallengeApp.swift
+++ GS-CodingChallenge/AppSetup/GS_CodingChallengeApp.swift
@@ -11,7 +11,16 @@ import SwiftUI
 struct GS_CodingChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView().environment(\.colorScheme, .light)
+            MainView().modifier(DarkModeViewModifier())
         }
+    }
+}
+
+public struct DarkModeViewModifier: ViewModifier {
+@AppStorage("isDarkMode") var isDarkMode: Bool = true
+public func body(content: Content) -> some View {
+    content
+        .environment(\.colorScheme, isDarkMode ? .dark : .light)
+        .preferredColorScheme(isDarkMode ? .dark : .light)
     }
 }
--- GS-CodingChallenge/PictureOfTheDay/Controller/PictureOfTheDay.swift
+++ GS-CodingChallenge/PictureOfTheDay/Controller/PictureOfTheDay.swift
@@ -12,6 +12,7 @@ struct PictureOfTheDay: View {
     @ObservedObject var viewModel = PictureOfTheDayViewModel()
     @StateObject var selectedDate = PassDateObject(Date())
     @State private var showingPopover = false
+    @AppStorage("isDarkMode") var isDarkMode: Bool = true
     
     var body: some View {
         NavigationView {
@@ -22,13 +23,20 @@ struct PictureOfTheDay: View {
             } .onAppear {
                 self.viewModel.getPictureOfTheDay()
             }.navigationTitle("Picture Of The Day")
-                .navigationBarTitleDisplayMode(.inline).toolbar {
-                    Button {
-                        showingPopover = true
-                    } label: {
-                        Image(systemName: "calendar")
-                    }
-                }.popover(isPresented: $showingPopover, attachmentAnchor: .point(.bottomTrailing), arrowEdge: .trailing) {
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(leading:
+                                        Button {
+                    isDarkMode = isDarkMode == true ? false : true
+                } label: {
+                    Image(systemName: "gearshape.fill")
+                },
+                                    trailing:
+                                        Button {
+                    showingPopover = true
+                } label: {
+                    Image(systemName: "calendar")
+                }
+                ).popover(isPresented: $showingPopover, attachmentAnchor: .point(.bottomTrailing), arrowEdge: .trailing) {
                     iOSDatePicker(updateDetail: self.fetchPictureOfTheDayForSelectedDate).environmentObject(selectedDate)
                 }
         }
